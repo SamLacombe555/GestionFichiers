5d04230e2d8d7f8427addbaad6ffa0c9e9c377ec
2233299 <2233299@cegepoutaouais.qc.ca>
2026-09-15 15:46:21 -0400

    premier push/commit

--- DemoGestionDeFichiers/Program.cs
+++ DemoGestionDeFichiers/Program.cs
@@ -8,7 +8,42 @@ string fichier = Path.Combine(basePath, "Data", "livres.json");
 
 string? dossier = Path.GetDirectoryName(fichier);
 
+if (dossier is not null && Directory.Exists(dossier))
+{
+    Directory.CreateDirectory(dossier);
+}
+
+static void CreerLivreJson(string fichier)
+{
+    //List<Livre> livres = new List<Livre>();
+
+    //livres.Add(new Livre("Clean ARchitecture", "Robert C. Martin", 2017));
+
+    List<Livre> livres = new List<Livre>
+    {
+        new Livre("Clean ARchitecture", "Robert C. Martin", 2017),
+        new Livre("The Pragmatic Programmer", "David Thomas et Andrew Hunt", 2019),
+        new Livre("Fundamentals of Software Architecture", "Mark Richards et Neal Ford", 2020)
+    };
+
+    JsonSerializerOptions options = new JsonSerializerOptions { WriteIndented = true };
+    string livresJson = JsonSerializer.Serialize(livres);
+    File.WriteAllText(fichier, livresJson);
+
+
+}
+
+static void LireLivresJson(string fichier)
+{
+    string livreJson = File.ReadAllText(fichier);
+    List<Livre>? livres = JsonSerializer.Deserialize<List<Livre>>(livreJson);
+
+
+
+}
 
 //"Clean Architecture", "Robert C. Martin", 2017
 //"The Pragmatic Programmer", "David Thomas et Andrew Hunt", 2019
 //"Fundamentals of Software Architecture", "Mark Richards et Neal Ford", 2020
+
+
