d38e5328875a31663ccff71ce588bcfbf9723023
SamLacombe555 <2233299@cegepoutaouais.qc.ca>
2026-09-15 15:11:15 -0400

    Initial commit

---
+++ DemoGestionDeFichiers/Livre.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace DemoGestionDeFichiers
+{
+    public class Livre
+    {
+        public string Titre { get; set; }
+        public string Auteur { get; set; }
+        public int Annee { get; set; }
+
+        public Livre(string titre, string auteur, int annee)
+        {
+            Titre = titre;
+            Auteur = auteur;
+            Annee = annee;
+        }
+
+        public override string ToString()
+        {
+            return $"{Titre} ({Annee}), {Auteur}";
+        }
+       
+    }
+}
---
+++ DemoGestionDeFichiers/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using DemoGestionDeFichiers;
+using System.Text.Json;
+
+// Pour l'avoir dans votre projet 
+string basePath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+string fichier = Path.Combine(basePath, "Data", "livres.json");
+
+string? dossier = Path.GetDirectoryName(fichier);
+
+
+//"Clean Architecture", "Robert C. Martin", 2017
+//"The Pragmatic Programmer", "David Thomas et Andrew Hunt", 2019
+//"Fundamentals of Software Architecture", "Mark Richards et Neal Ford", 2020
